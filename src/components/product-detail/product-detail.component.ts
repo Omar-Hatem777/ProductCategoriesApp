11d96281fe83f10df2ae0a81f22747e2b1853a86
Omar-Hatem777 <omarhatemun11@gmail.com>
2025-08-23 00:29:59 +0300

    -update styles for dark mode. -add navbar in app component. -updated README. - handled logic and styling for all components

--- src/components/product-detail/product-detail.component.ts
+++ src/components/product-detail/product-detail.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { IProduct } from '../../models/product';
+import { CommonModule, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-product-detail',
   standalone: true,
-  imports: [],
+  imports: [NgIf, CommonModule, RouterLink],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.css'
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit {
 
+  constructor(private productService: ProductService, private activatedRoute: ActivatedRoute) { } // Inject ActivatedRoute and ProductService
+
+  product?: IProduct; // Initialize product object
+  productId: number = 0; // Initialize productId
+
+  ngOnInit(): void {
+    this.productId = Number(this.activatedRoute.snapshot.paramMap.get('productId')); // Get productId from route parameters
+    console.log(this.productId); // Log productId to verify integration
+    this.product = this.productService.getProductById(this.productId); // Fetch product by ID from ProductService
+  }
 }
--- src/components/product-list/product-list.component.ts
+++ src/components/product-list/product-list.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { IProduct } from '../../models/product';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
 
+  constructor(private productService: ProductService, private activatedRoute: ActivatedRoute) { } // Inject ActivatedRoute and ProductService
+
+  products: IProduct[] = []; // Initialize products array
+  category: string = '';
+
+  ngOnInit(): void {
+    this.category = this.activatedRoute.snapshot.paramMap.get('categoryName') || ''; // Get categoryName from route parameters
+    console.log(this.category); // Log category to verify integration
+    this.products = this.productService.getProductsByCategory(this.category);
+    console.log(this.products); // Log products to verify integration
+  }
 }
